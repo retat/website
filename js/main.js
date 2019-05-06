8cfdf2d6596b1df0e30dab16b92f51222fcc531b
Rene Borner <mail@reneborner.de>
2019-05-06 20:24:10 +0200

    imported jQuery

--- js/main.js
+++ js/main.js
@@ -1,10 +1,3 @@
 function toggleSocial(){
-    var element = document.getElementById("social");
-    if (element.classList.contains("disabled")){
-        element.classList.remove("disabled");
-        element.classList.add("enabled");
-    } else {
-        element.classList.remove("enabled");
-        element.classList.add("disabled");
-    }
+    $("#social").fadeToggle();
 }
