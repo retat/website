2b1f18e06f0f91ae0078b9ad752945353283620e
René Borner <mail@reneborner.de>
2025-01-02 12:49:42 +0100

    added charging calculator

--- js/main.js
+++ js/main.js
@@ -1,42 +1,37 @@
-const popupElement = $("#popup")
+function calculateTime() {
+  const batterySize = parseFloat(document.getElementById("battery").value);
+  const currentCharge = parseFloat(
+    document.getElementById("current-charge").value,
+  );
+  const chargingGoal = parseFloat(
+    document.getElementById("charging-goal").value,
+  );
+  const chargingSpeed = parseFloat(
+    document.getElementById("charging-speed").value,
+  );
 
-function showSocial() {
-    togglePopup(
-        "        <div class=\"popupContent\">\n" +
-        "        <p>Stalk me on:</p>\n" +
-        "        <a href=\"https://twitter.com/Rene__Borner\">Twitter</a><br>\n" +
-        "        <a href=\"https://github.com/retat\">Github</a><br>\n" +
-        "        <a href=\"https://www.instagram.com/rene______b/\">Instagram</a>\n" +
-        "    </div>", "social")
-}
+  if (currentCharge >= chargingGoal) {
+    document.getElementById("minutes").innerText = "0";
+    document.getElementById("hours").innerText = "0:00";
+    document.getElementById("finish-time").innerText = "Jetzt";
+    return;
+  }
 
-function showProjects() {
-    togglePopup(
-        "        <div class=\"popupContent\">\n" +
-        "        <p>Some projects:</p>\n" +
-        "        <a href=\"http://ai.reneborner.de\">AI Presentation</a><br>\n" +
-        "        <a href=\"https://cs4241-best-group-fp-7.glitch.me/\">Music Recommendations Platform</a><br>\n" +
-        "        <a href=\"https://github.com/retat/AI-TranslationModelServer\">English to Java AI Project</a><br>\n" +
-        "        <a href=\"https://retat.github.io/audiofile-visualizer/\">Audiofile Visualizer</a>\n" +
-        "    </div>", "projects")
-}
+  const chargeNeeded = ((chargingGoal - currentCharge) / 100) * batterySize;
+  const timeInMinutes = (chargeNeeded / chargingSpeed) * 60;
 
-function showCV() {
-    togglePopup(
-        "        <div class=\"popupContent\">\n" +
-        "        <p>Résumé:</p>\n" +
-        "        <p>CV currently not available online</p><br>\n" +
-        "    </div>", "cv")
-}
+  const hours = Math.floor(timeInMinutes / 60);
+  const minutes = Math.round(timeInMinutes % 60);
 
-function togglePopup(content, type) {
-    if (popupElement.hasClass("active") && popupElement.hasClass(type)) {
-        popupElement.fadeOut().html(content)
-        popupElement.removeClass("active").removeClass(type);
-    } else if (popupElement.hasClass("active") && !popupElement.hasClass(type)) {
-        popupElement.removeClass()
-        popupElement.fadeIn().html(content).addClass("active").addClass(type).addClass("popup")
-    }else {
-        popupElement.fadeIn().html(content).addClass("active").addClass(type)
-    }
+  const now = new Date();
+  const finishTime = new Date(now.getTime() + timeInMinutes * 60000);
+  const finishTimeFormatted = finishTime.toLocaleTimeString("de-DE", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+
+  // Update table
+  document.getElementById("time").innerText =
+    `${hours}:${minutes.toString().padStart(2, "0")}h`;
+  document.getElementById("finish-time").innerText = finishTimeFormatted;
 }
